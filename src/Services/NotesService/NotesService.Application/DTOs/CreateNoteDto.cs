a5609fa334aa613ed6940d2fc5d2ac97b738e7e3
Manish Kumar Kaushal <manishdn2003@gmail.com>
2026-06-22 21:35:38 +0530

    Added NotesService application folders

---
+++ src/Services/NotesService/NotesService.Application/DTOs/CreateNoteDto.cs
@@ -0,0 +1,8 @@
+﻿namespace NotesService.Application.DTOs;
+
+public class CreateNoteDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public string Color { get; set; } = "#FFFFFF";
+}
---
+++ src/Services/NotesService/NotesService.Application/Interfaces/INoteRepository.cs
@@ -0,0 +1,8 @@
+﻿using NotesService.Domain.Entities;
+
+namespace NotesService.Application.Interfaces;
+
+public interface INoteRepository
+{
+    Task<int> AddAsync(Note note);
+}
