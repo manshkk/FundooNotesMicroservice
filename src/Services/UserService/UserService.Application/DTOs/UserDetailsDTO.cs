1cea671ba1a2ca3ac973c7a1bd6aaa83499df331
Manish Kumar Kaushal <manishdn2003@gmail.com>
2026-06-24 13:13:39 +0530

    Added get user by email query contract

---
+++ src/Services/UserService/UserService.Application/DTOs/UserDetailsDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace UserService.Application.DTOs;
+
+public class UserDetailsDTO
+{
+    public int UserId { get; set; }
+
+    public string Email { get; set; } = string.Empty;
+}
---
+++ src/Services/UserService/UserService.Application/Queries/GetUserByEmail/GetUserByEmailQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using UserService.Application.DTOs;
+
+namespace UserService.Application.Queries.GetUserByEmail;
+
+public record GetUserByEmailQuery(string Email)
+    : IRequest<UserDetailsDTO?>;
