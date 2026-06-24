5f52536dcb56d7a4132f3dce461f8bacae98d606
Manish Kumar Kaushal <manishdn2003@gmail.com>
2026-06-24 14:49:58 +0530

    Added get collaborators endpoint

--- src/Services/CollaborationService/CollaborationService.API/Controllers/CollaboratorsController.cs
+++ src/Services/CollaborationService/CollaborationService.API/Controllers/CollaboratorsController.cs
@@ -1,6 +1,44 @@
-﻿namespace CollaborationService.API.Controllers
+﻿using CollaborationService.Application.Commands.AddCollaborator;
+using CollaborationService.Application.DTOs;
+using CollaborationService.Application.Queries.GetCollaborators;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollaborationService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CollaboratorsController : ControllerBase
 {
-    public class CollaboratorsController
+    private readonly IMediator _mediator;
+
+    public CollaboratorsController(IMediator mediator)
     {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddCollaborator(
+        AddCollaboratorRequestDto dto)
+    {
+        var result = await _mediator.Send(
+            new AddCollaboratorCommand(dto));
+
+        if (!result)
+        {
+            return BadRequest("Collaborator not found.");
+        }
+
+        return Ok("Collaborator added successfully.");
+    }
+
+    [HttpGet("{noteId}")]
+    public async Task<IActionResult> GetCollaborators(
+        int noteId)
+    {
+        var collaborators = await _mediator.Send(
+            new GetCollaboratorsQuery(noteId));
+
+        return Ok(collaborators);
     }
 }
