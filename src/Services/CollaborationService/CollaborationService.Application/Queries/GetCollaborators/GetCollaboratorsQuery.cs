49170175b8e7673bdbf03c5231ff973dbc2ebd95
Manish Kumar Kaushal <manishdn2003@gmail.com>
2026-06-24 14:37:11 +0530

    Added get collaborators query

---
+++ src/Services/CollaborationService/CollaborationService.Application/Queries/GetCollaborators/GetCollaboratorsQuery.cs
@@ -0,0 +1,7 @@
+﻿using CollaborationService.Domain.Entities;
+using MediatR;
+
+namespace CollaborationService.Application.Queries.GetCollaborators;
+
+public record GetCollaboratorsQuery(int NoteId)
+    : IRequest<List<Collaborator>>;
