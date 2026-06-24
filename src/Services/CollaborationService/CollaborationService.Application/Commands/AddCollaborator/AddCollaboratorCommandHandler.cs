15e62842c867fc297348fc976670e1664218a146
Manish Kumar Kaushal <manishdn2003@gmail.com>
2026-06-24 13:07:58 +0530

    Added collaborator command handler dependencies

--- src/Services/CollaborationService/CollaborationService.Application/Commands/AddCollaborator/AddCollaboratorCommandHandler.cs
+++ src/Services/CollaborationService/CollaborationService.Application/Commands/AddCollaborator/AddCollaboratorCommandHandler.cs
@@ -1,11 +1,24 @@
-﻿using MediatR;
+﻿using CollaborationService.Application.Interfaces;
+using CollaborationService.Domain.Entities;
+using MediatR;
 
 namespace CollaborationService.Application.Commands.AddCollaborator;
 
 public class AddCollaboratorCommandHandler
     : IRequestHandler<AddCollaboratorCommand, bool>
 {
-    public Task<bool> Handle(
+    private readonly IUserServiceClient _userServiceClient;
+    private readonly ICollaboratorRepository _collaboratorRepository;
+
+    public AddCollaboratorCommandHandler(
+        IUserServiceClient userServiceClient,
+        ICollaboratorRepository collaboratorRepository)
+    {
+        _userServiceClient = userServiceClient;
+        _collaboratorRepository = collaboratorRepository;
+    }
+
+    public async Task<bool> Handle(
         AddCollaboratorCommand request,
         CancellationToken cancellationToken)
     {
