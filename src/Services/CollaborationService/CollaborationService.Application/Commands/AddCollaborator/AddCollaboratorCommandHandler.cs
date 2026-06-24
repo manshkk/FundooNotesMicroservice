025cd71ac4e72741bceb17ccdbd11ab8c6f31586
Manish Kumar Kaushal <manishdn2003@gmail.com>
2026-06-24 13:06:20 +0530

    Added collaborator command handler skeleton

--- src/Services/CollaborationService/CollaborationService.Application/Commands/AddCollaborator/AddCollaboratorCommandHandler.cs
+++ src/Services/CollaborationService/CollaborationService.Application/Commands/AddCollaborator/AddCollaboratorCommandHandler.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
-namespace CollaborationService.Application.Commands.AddCollaborator
+namespace CollaborationService.Application.Commands.AddCollaborator;
+
+public class AddCollaboratorCommandHandler
+    : IRequestHandler<AddCollaboratorCommand, bool>
 {
-    internal class AddCollaboratorCommandHandler
+    public Task<bool> Handle(
+        AddCollaboratorCommand request,
+        CancellationToken cancellationToken)
     {
+        throw new NotImplementedException();
     }
 }
