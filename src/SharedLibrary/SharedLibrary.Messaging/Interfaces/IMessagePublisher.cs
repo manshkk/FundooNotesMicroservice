7053d30462c7ddf6e6bb23f624e77f7de418c41c
Manish Kumar Kaushal <manishdn2003@gmail.com>
2026-06-25 16:32:01 +0530

    Added SharedLibrary.Messaging foundation

---
+++ src/SharedLibrary/SharedLibrary.Messaging/Interfaces/IMessagePublisher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary.Messaging.Interfaces
+{
+    internal class IMessagePublisher
+    {
+    }
+}
---
+++ src/SharedLibrary/SharedLibrary.Messaging/Publishers/RabbitMqPublisher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibrary.Messaging.Publishers
+{
+    internal class RabbitMqPublisher
+    {
+    }
+}
