35342e151f6cce44c183904876945b12ca16f647
Manish Kumar Kaushal <manishdn2003@gmail.com>
2026-06-25 14:32:06 +0530

    Implemented Redis caching for GetAllNotes query

---
+++ src/SharedLibrary/SharedLibrary.Caching/Constants/CacheKeys.cs
@@ -0,0 +1,11 @@
+﻿public static class CacheKeys
+{
+    public static string UserNotes(int userId)
+        => $"notes:user:{userId}";
+
+    public static string TrashNotes(int userId)
+        => $"notes:trash:user:{userId}";
+
+    public static string NoteById(int noteId)
+        => $"note:{noteId}";
+}
