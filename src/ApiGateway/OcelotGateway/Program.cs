f8e28722e684d4aab7e47700f0e56ad64095ec79
Manish Kumar Kaushal <manishdn2003@gmail.com>
2026-07-15 17:19:14 +0530

    we added cros in the api gateway

--- src/ApiGateway/OcelotGateway/Program.cs
+++ src/ApiGateway/OcelotGateway/Program.cs
@@ -3,15 +3,27 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration
-    .AddJsonFile(
-        "ocelot.json",
-        optional: false,
-        reloadOnChange: true);
+builder.Configuration.AddJsonFile(
+    "ocelot.json",
+    optional: false,
+    reloadOnChange: true);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactPolicy", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddOcelot();
 
 var app = builder.Build();
+
+app.UseCors("ReactPolicy");
 
 await app.UseOcelot();
 
